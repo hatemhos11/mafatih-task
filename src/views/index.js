bf6c93656b0d7290c4944f44608ab44219d0166c
hatemhos11 <hatemhosni00@gmail.com>
2023-10-01 12:16:53 +0300

    remove about page

--- src/views/index.js
+++ src/views/index.js
@@ -1,11 +1,9 @@
 import Home from './Home.vue'
-import About from './About.vue'
 import Mng from './Mng.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/about', component: About },
   { path: '/mng', component: Mng },
 ]
 
